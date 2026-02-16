bd3894c002c1016c1937135460749b44b6926642
orient-master <arunbhandare101@gmail.com>
2026-02-16 23:01:36 +0545

    WIP: AlertApp

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import React from 'react';
 // import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
 // import RandomizeColor from './projects/02-randomize-colors/RandomizeColor';
 // import LikeMyPhotoApp from './projects/03-like-my-photo/LikeMyPhotoApp';
-import TestimonialsApp from './projects/04-testimonials/TestimonialsApp';
+// import TestimonialsApp from './projects/04-testimonials/TestimonialsApp';
+import AlertApp from './projects/05-alerts-app/AlertApp';
 
 function App() {
   return (
     <div className="App">
 
-       {/* Projects 
-      <EsignatureApp />
-      < RandomizeColor />
-      <LikeMyPhotoApp /> */}
-
-      <TestimonialsApp />
-      
+      {/* Projects */} 
+      {/* <EsignatureApp /> */}
+      {/* < RandomizeColor /> */}
+      {/* <LikeMyPhotoApp /> */}
+      {/* <TestimonialsApp /> */}
+      <AlertApp />
 
     </div>
   );
---
+++ src/projects/05-alerts-app/AlertApp.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Title from '../components/Title';
+
+export default function AlertApp() {
+  return (
+    <div className='container text-center'>
+      <Title text={'Alert App'} classes={'title-main'} />
+
+      
+    </div>
+  )
+}
