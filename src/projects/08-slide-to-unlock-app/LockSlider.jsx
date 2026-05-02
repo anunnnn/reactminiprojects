296c3c32df06e042c1cc47309dbbc77104a77665
orient-master <arunbhandare101@gmail.com>
2026-05-02 15:06:50 +0545

    Add slideToUnlockApp

--- src/projects/08-slide-to-unlock-app/LockSlider.jsx
+++ src/projects/08-slide-to-unlock-app/LockSlider.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './LockSlider.css';
 
-export default function LockSlider() {
+export default function LockSlider({handleInput, sliderValue, width}) {
+
+  let sliderStyle = {
+    appearance: 'none',
+    width: !width ? '300px' : width,
+    height: '50px',
+    background: 'rgba(180, 190, 188, 0.5)',
+    outline: 'none',
+    borderRadius: '25px'
+  }
+
+
   return (
     <div>
-      
+      <input
+        type="range"
+        className='slider mb-2'
+        style={sliderStyle}
+        value={sliderValue}
+        onChange={handleInput}
+      />
     </div>
   )
 }
--- src/projects/08-slide-to-unlock-app/SlideToUnlockApp.jsx
+++ src/projects/08-slide-to-unlock-app/SlideToUnlockApp.jsx
@@ -1,12 +1,61 @@
-import React from 'react';
-import Title from '../components/Title';
+import React, { useState, useEffect } from 'react';
 import LockSlider from './LockSlider';
+import { AiFillUnlock } from 'react-icons/ai';
+
+import LockScreenImg from './images/road.jpg';
+import HomeScreenImg from './images/beach.jpg';
 
 export default function SlideToUnlockApp() {
+
+  const [uiProps, setUiProps] = useState({
+    uiText:'Unlock screen',
+    uiColor: '#eee',
+    uiBg: `url(${LockScreenImg}) center/cover no-repeat`
+  });
+
+  const [showLockSlider, setShowLockSlider] = useState(true);
+  const [lockSliderValue, setLockSliderValue] = useState(0);
+
+  const handleLockSliderInput = (e) => {
+    setLockSliderValue(e.target.value);
+  }
+  
+  useEffect(() => {
+    if(lockSliderValue == 100 ){
+      setShowLockSlider(false);
+      setUiProps({
+        uiText:'Unlocked Screen',
+        uiColor: '#eee',
+        uiBg: `url(${HomeScreenImg}) center/cover no-repeat`
+      })
+    }
+  }, [lockSliderValue]);
+
+
   return (
-    <div className='container text-center'>
-        <Title text='Slide to Unlock App' />
-        <LockSlider />
+    <div className='container text-center d-flex flex-column border-20 shadow-md '
+      style={{
+        height: '70vh',
+        width: 340,
+        marginTop: '15vh',
+        border: '4px solid #000',
+        background: uiProps.uiBg      }}>
+
+        <h1 className='title' style={{ color: uiProps.uiColor }}>{uiProps.uiText}</h1>
+        {
+          showLockSlider
+          ?
+          (
+            <LockSlider 
+              handleInput={handleLockSliderInput} 
+              sliderValue={lockSliderValue} 
+              width={'250px'} />
+          )
+          :
+          (
+            <AiFillUnlock className='unlockIcon' />
+          )
+        }
       
     </div>
   )
